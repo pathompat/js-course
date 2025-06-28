8f090e54a889977e599978881ac0bad1d98b0b51
pathompat <lw93430@linecorp.com>
2025-06-29 00:55:35 +0700

    add vue app

---
+++ vue3-app/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <main>
+    <HelloWorld first-name="Pathompat" last-name="Sungpankhao" />
+  </main>
+</template>
---
+++ vue3-app/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import { ref, computed, watch, defineProps } from 'vue'
+
+  const props = defineProps({
+    firstName: String,
+    lastName: String
+  })
+  const count = ref(0)
+  const items = ref(['Item 1', 'Item 2', 'Item 3'])
+
+  const fullName = computed(() => {
+    return `${props.firstName} ${props.lastName}`
+  })
+
+  const incremental = () => {
+    count.value++
+  }
+
+  watch(count, (newVal, oldVal) => {
+    console.log(`คลิกเปลี่ยนจาก ${oldVal} เป็น ${newVal}`)
+  })
+</script>
+
+<template>
+  <div>
+    <h2>Hello, {{ fullName }}!</h2>
+    <p>Clicked: {{ count }}</p>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="incremental">Click</button>
+  </div>
+</template>
